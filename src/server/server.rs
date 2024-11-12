a59717e0e45d7f7ca67a99131c30b3a34c342eef
Felipe-Devr <felipegamerdev1@gmail.com>
2024-11-12 00:34:41 -0700

    feat: basic test ping / pong

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,6 +1,9 @@
-use std::{fmt::Error, net::SocketAddr};
+use std::{fmt::Error, io::Cursor, net::SocketAddr, time::UNIX_EPOCH};
 
+use byteorder::ReadBytesExt;
 use tokio::net::UdpSocket;
+
+use crate::protocol::{Packet, UnconnectedPing, UnconnectedPong};
 
 pub struct RakNetConfiguration {
 	pub address: SocketAddr
@@ -10,7 +13,7 @@ pub struct RakNetServer {
 	pub address: SocketAddr,
     pub connections: Vec<u8>,
     pub socket: UdpSocket,
-    pub guid: i64
+    pub guid: u64
 }
 
 impl RakNetServer {
@@ -26,9 +29,27 @@ impl RakNetServer {
     }
 
 	pub async fn listen(&self) -> Result<bool, Error> {
+		println!("Server listening on port {}\n", self.address.port());
 		loop {
 			let mut buffer = [0; 4096];
-            let (size, peer_addr) = self.socket.recv_from(&mut buffer).await.unwrap();
+            let (_, peer_addr) = self.socket.recv_from(&mut buffer).await.unwrap();
+			let mut cursor = Cursor::new(Vec::from(buffer));
+			let packet_id = cursor.read_u8().expect("Failled to read packet id.");
+
+			println!("Received packet with ID: {} from {}", packet_id, peer_addr);
+
+			if packet_id == UnconnectedPing::ID {
+				let response = UnconnectedPong {
+					id: String::from("MCPE;Rustified!;100;1.0.0;0;10;3242348208447;Rust Raknet;Survival;1;19142;19143;"),
+					timestamp: std::time::SystemTime::now().duration_since(UNIX_EPOCH).unwrap(),
+                    server_guid: self.guid
+				};
+
+				println!("{:?}", response);
+				let mut response_buf: Vec<u8> = Vec::new();
+                response.serialize(&mut response_buf);
+                self.socket.send_to(&response_buf, peer_addr).await.expect("Failed to send response");
+			}
 		}
 	}
 }
