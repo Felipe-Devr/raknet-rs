17a4ebb9e4d8f0c1876545768a396769221299d3
Felipe-Devr <felipegamerdev1@gmail.com>
2024-11-13 14:51:34 -0700

    feat: added from to Address and fixed parsing

--- src/misc/address.rs
+++ src/misc/address.rs
@@ -1,4 +1,6 @@
-use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
+use std::net::{SocketAddr, IpAddr};
+
+use super::{BinaryStream, Endianness};
 
 pub enum AddressVersion {
     IPv4,
@@ -12,10 +14,21 @@ pub struct Address {
 }
 
 impl Address {
-    pub fn serialize(&self, buffer: &mut Vec<u8>) {
+    pub fn from(sockaddr: &SocketAddr) -> Address {
+        Address {
+            version: match sockaddr.ip() {
+                IpAddr::V4(_) => AddressVersion::IPv4,
+                IpAddr::V6(_) => AddressVersion::IPv6,
+            },
+            ip: sockaddr.ip().to_string(),
+            port: sockaddr.port(),
+        }
+    }
+
+    pub fn serialize(&self, buffer: &mut BinaryStream) {
         match self.version {
             AddressVersion::IPv4 => {
-                buffer.write_u8(4).unwrap();
+                buffer.write_u8(4);
                 let octates: Vec<u8> = self
                     .ip
                     .split('.')
@@ -23,12 +36,12 @@ impl Address {
                     .collect();
 
 				for octate in octates {
-					buffer.write_u8(octate).unwrap();
+					buffer.write_u8(octate);
 				}
-				buffer.write_u16::<BigEndian>(self.port).unwrap();
+				buffer.write_u16(self.port, Endianness::BigEndian);
             }
             AddressVersion::IPv6 => {
-                buffer.write_u8(6).unwrap();
+                buffer.write_u8(6);
                 /*  buffer.write_u8(16).unwrap();
                 buffer.write_all(self.ip.as_bytes()).unwrap();
                 buffer.write_u16::<BigEndian>(self.port).unwrap(); */
@@ -36,18 +49,18 @@ impl Address {
         }
     }
 
-	pub fn deserialize(buffer: &mut std::io::Cursor<Vec<u8>>) -> Option<Self> {
-		let version = buffer.read_u8().unwrap();
-
+	pub fn deserialize(buffer: &mut BinaryStream) -> Option<Self> {
+        let version = buffer.read_u8().unwrap();
+        
 		match version {
-			4 => {
+			4 | 0 => {
 				let mut octets: Vec<u8> = Vec::new();
 
-				for _ in 0..3 {
+				for _ in 0..4 {
 					octets.push(buffer.read_u8().unwrap());
-				}
-				let port = buffer.read_u16::<BigEndian>().unwrap();
-				
+				}                
+				let port = buffer.read_u16(Endianness::BigEndian).unwrap();
+			
 				Some(Address {
                     version: AddressVersion::IPv4,
                     ip: octets.iter().map(|x| format!("{}", x)).collect::<Vec<String>>().join("."),
